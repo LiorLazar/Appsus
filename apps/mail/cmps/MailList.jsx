6f626f39942f442e698ff1e13ef11b7e0f37c6a7
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-07-01 14:05:50 +0300

    Add Unread Mails Counter to the Mails List

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -3,8 +3,11 @@ import { MailPreview } from "./MailPreview.jsx"
 const { Link } = ReactRouterDOM
 
 export function MailList({ mails }) {
+    const unreadCount = mails.filter(mail => !mail.isRead).length
+
     return (
         <div className="mail-list container">
+            <div>Unread Mails: {unreadCount}</div>
             <Link to="compose" className="compose-btn">+ Compose</Link>
             {mails.map(mail =>
                 <Link key={mail.id} to={mail.id}>
