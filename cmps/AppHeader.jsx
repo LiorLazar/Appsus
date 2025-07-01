b8be3c475662c72ecec69fc6e4da8302dc0efd97
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-07-01 19:40:34 +0300

    Implement Header Styling

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -4,14 +4,34 @@ export function AppHeader() {
     const location = useLocation()
     const isMail = location.pathname.includes("/mail");
     const isNote = location.pathname.includes("/note");
+
     return (
         <header className="app-header">
-            <Link to="/">
-                {
-                    isMail ?
-                        <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png" />
-                        : <div><span>Keep</span> <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"></img></div>}
+            <Link to="/" className="logo">
+                {isMail ? (
+                    <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png" />
+                ) : (
+                    <div className="keep-logo">
+                        <span>Keep</span>
+                        <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" />
+                    </div>
+                )}
             </Link>
+
+            <div className="header-bar">
+                <div className="search-bar">
+                    <span className="material-symbols-outlined">search</span>
+                    <input type="text" placeholder="Search mail" />
+                    <span className="material-symbols-outlined">tune</span>
+                </div>
+                <div className="header-icons">
+                    <span className="material-symbols-outlined">help</span>
+                    <span className="material-symbols-outlined">settings</span>
+                    <span className="material-symbols-outlined">apps</span>
+                    <img className="avatar" src="https://cdn.vectorstock.com/i/750p/51/99/user-avatar-icon-flat-style-vector-3125199.avif" alt="avatar" />
+                </div>
+            </div>
+
             <nav>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/about">About</NavLink>
