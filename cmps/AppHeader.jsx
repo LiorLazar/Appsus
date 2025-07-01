8f2b2d51da83697403aa6fb0dc5c70b9257a2dc6
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-07-01 18:52:52 +0300

    Implement Logo

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,23 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink, useLocation } = ReactRouterDOM
 
 export function AppHeader() {
-
-    return <header className="app-header">
-        <Link to="/">
-            <h3>LOGO ✨</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
-    </header>
+    const location = useLocation()
+    const isMail = location.pathname.includes("/mail");
+    const isNote = location.pathname.includes("/note");
+    return (
+        <header className="app-header">
+            <Link to="/">
+                {
+                    isMail ?
+                        <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png" />
+                        : <div><span>Keep</span> <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"></img></div>}
+            </Link>
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/mail">Mail</NavLink>
+                <NavLink to="/note">Note</NavLink>
+            </nav>
+        </header>
+    )
 }
