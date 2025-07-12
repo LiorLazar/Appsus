dc08a9cc1760e4cd9f8cb3979ef476c65076021b
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-07-12 18:14:24 +0300

    Fix Home img pathes

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -7,10 +7,10 @@ export function Home() {
         <h4>Center All Your Everyday Apps Into One Location</h4>
         <div className='apps-container'>
             <Link to='mail/inbox'>
-                <img className="mail-logo" src="../assets/img/gmail-logo.webp" alt="Gmail" />
+                <img className="mail-logo" src="./assets/img/gmail-logo.webp" alt="Gmail" />
             </Link>
             <Link to="note">
-                <img className="keep-logo-home" src="../assets/img/keep-logo.png" alt="Google Keep" />
+                <img className="keep-logo-home" src="./assets/img/keep-logo.png" alt="Google Keep" />
             </Link>
         </div>
         <Link className="about-btn" to='/about'>About</Link>
